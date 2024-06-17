2f029701f07ef0d617739cef5ff1fa2fd0f78a54
zzetsubouu <154281553+zzetsubouu@users.noreply.github.com>
2024-06-17 13:44:49 +0300

    Add files via upload

---
+++ Full Perechen/Full Perechen/Program.cs
@@ -0,0 +1,510 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Full_Perechen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача 1
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}+{y}={x+y}");
+
+            // Задача 2 
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.Write("z=");
+            //int z = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}+{y}+{z}={x + y + z}");
+
+            // Задача 3
+            //Console.Write("x=");
+            //double x = double.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //double y = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}*{y}={x * y:F1}");
+
+            // Задача 4
+            //Console.Write("x=");
+            //double x = double.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //double y = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x:F3}/{y:F3}={x/y:F3}");
+
+            // Задача 5
+            //Console.Write("a=");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}+{y}={y}+{x}");
+
+            // Задача 6
+            //Console.Write("a=");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("b=");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"a+b={x}+{y}={x + y}\ta-b={x}-{y}={x - y}\ta*b={x}*{y}={x * y}");
+
+            // Задача 7
+            //Console.Write("Как вас зовут? ");
+            //string name = Console.ReadLine();
+            //Console.Write("Сколько тебе лет? ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.Write("Было ли день рождение у тебя в этом году?");
+            //string answer = Console.ReadLine();
+            //if(answer=="да"||answer=="Да")
+            //{
+            //    Console.WriteLine($"{name}, ты родился в {DateTime.Now.Year-age} году");
+            //}
+            //else if(answer=="нет"||answer=="Нет")
+            //{
+            //    Console.WriteLine($"{name}, ты родился в {DateTime.Now.Year - age-1} году");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Неправильный ответ");
+            //}
+
+            // Задача 8 
+            //Console.Write("Номинал купюры = ");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("Количество купюр = ");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Сумма денег = {x * y:F2}р.");
+
+            // Задача 9 
+            //Console.Write("Введите сторону квадрата: ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Периметр квадрата равен {4*a}");
+
+            // Задача 10
+            //Console.Write("Введите сторону a: ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("Введите сторноу b: ");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Периметр прямоугольника: {2*(a+b)}");
+
+            // Задача 11
+            //Console.Write("Введите длину радиуса: ");
+            //double r = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"Длина окружности: {2*Math.PI*r:F2}");
+
+            // Задача 12
+            //Console.WriteLine("Введите объём в ГБ: ");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Файлов: {(x*1024)/820}");
+
+            // Задача 13
+            //Console.Write("Введите двузначное число: ");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x/10} и {x%10}");
+
+            // Задача 14
+            //double y;
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x >= 0)
+            //{
+            //    y = Math.Sqrt(x) + 2 * Math.Sin(x);
+            //}
+            //else
+            //{
+            //    y = 6 - Math.Pow(x, 2);
+            //}
+            //Console.WriteLine($"y={y:F2}");
+
+            // Задача 15
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //double y = 0;
+            //if (x<=-5)
+            //{
+            //    y = Math.Pow(x, 2) + 15 * x;
+            //}
+            //if (x>=5)
+            //{
+            //    y = (3 * x + 1) / 5;
+            //}
+            //if (x>-5 && x<5)
+            //{
+            //    y = Math.Sqrt(13 + x);
+            //}
+            //Console.WriteLine($"y={y}");
+
+            // Задача 16
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x > 0)
+            //{
+            //    x++;
+            //}
+            //else
+            //{
+            //    x = x - 2;
+            //}
+            //Console.WriteLine($"x={x}");
+
+            // Задача 17
+            //double y = 0;
+            //Console.WriteLine("Введите значение x: ");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x <= -5)
+            //{
+            //    y = Math.Sqrt(13 - x);
+            //}
+            //if (x >= 5)
+            //{
+            //    y = 3 * Math.Pow(x, 2) - 5 * x;
+            //}
+            //if (x > -5 && x < 5)
+            //{
+            //    y = Math.Abs(4 * x - 5);
+            //}
+            //Console.WriteLine(y);
+
+            // Задача 18
+            //Console.Write("Введите число: ");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x > 0)
+            //{
+            //    x++;
+            //}
+            //else
+            //{
+            //    x -= 2;
+            //}
+            //Console.WriteLine(x);
+
+            // Задача 19
+            //Console.Write("Введите число: ");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x > 0)
+            //{
+            //    x++;
+            //}
+            //if (x < 0)
+            //{
+            //    x -= 2;
+            //}
+            //if (x == 0)
+            //{
+            //    x = 10;
+            //}
+            //Console.WriteLine(x);
+
+            // Задача 20
+            //Console.Write("Введите число: ");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x > 0)
+            //{
+            //    Console.WriteLine("Ваше число является положительным!");
+            //}
+            //if (x < 0)
+            //{
+            //    Console.WriteLine("Ваше число является отрицательным!");
+            //}
+            //if (x == 0)
+            //{
+            //    Console.WriteLine("Ваше число является нулевым!");
+            //}
+
+            // Задача 21
+            //Console.Write("Введите первую сторону треугольника: ");
+            //double a = double.Parse(Console.ReadLine());
+            //Console.Write("Введите вторую сторону треугольника: ");
+            //double b = double.Parse(Console.ReadLine());
+            //Console.Write("Введите третью сторону треугольника: ");
+            //double c = double.Parse(Console.ReadLine());
+            //if (a==b && a==c)
+            //{
+            //    Console.WriteLine("Треугольник является равносторонним!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Треугольник не является равносторонним(");
+            //}
+
+            // Задача 22
+            //Console.Write("Введите число:");
+            //int x = int.Parse(Console.ReadLine());
+            //if (3 % x == 0)
+            //{
+            //    Console.WriteLine($"Число {x} является делителем числа 3.");
+            //}
+
+            // Задача 23
+            //Random rnd = new Random();
+            //for (int i = 0; i<10; i++)
+            //{
+            //    int x = rnd.Next(-30, 30);
+            //    if (x > 0)
+            //    {
+            //        Console.WriteLine($"{x} - положительное"); 
+            //    } 
+            //    else 
+            //    {
+            //        Console.WriteLine($"{x} - отрицательное"); 
+            //    }
+            //}
+
+            // Задача 24
+            //Random rnd = new Random();
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    int x = rnd.Next(-20, 30);
+            //    if (x % 2 == 0)
+            //    {
+            //        Console.WriteLine($"Число {x} - чётное");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Число {x} - нечётное");
+            //    }
+            //}
+
+            // Задача 25
+            //Random random = new Random();
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    int x = random.Next(-20, 20);
+            //    if (x > 0)
+            //    {
+            //        if (x % 2 == 0)
+            //        {
+            //            Console.WriteLine($"Число {x} - чётное");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine($"Число {x} - нечётное");
+            //        }
+            //    }
+
+            //}
+
+            // Задача 26
+            //Console.Write("Введите первое число: ");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("Введите второе число: ");
+            //int y = int.Parse(Console.ReadLine());
+            //if (x > y)
+            //{
+            //    Console.WriteLine($"Число {x} больше чем число {y}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Число {y} больше чем число {x}");
+            //}
+
+            // Задача #27
+            //Console.Write("Введите число от 1-7: ");
+            //int x = int.Parse(Console.ReadLine());
+            //switch (x)
+            //{
+            //    case 1: Console.WriteLine("Понедельник"); break;
+            //    case 2: Console.WriteLine("Вторник"); break;
+            //    case 3: Console.WriteLine("Среда"); break;
+            //    case 4: Console.WriteLine("Четверг"); break;
+            //    case 5: Console.WriteLine("Пятница"); break;
+            //    case 6: Console.WriteLine("Суббота"); break;
+            //    case 7: Console.WriteLine("Воскресенье"); break;
+            //    default: Console.WriteLine("Введите корректное число."); break;
+            //}
+
+            //Задача #28
+            //for (int x = 10; x <= 99; x++)
+            //{
+            //    if(x % 2 == 0)
+            //    {
+            //        Console.WriteLine(x);
+            //    }
+            //}
+
+            // Задача #29
+            //for (int x = 10; x <= 99; x++)
+            //{
+            //    if (x % 2 != 0)
+            //    {
+            //        Console.WriteLine(x);
+            //    }
+            //}
+
+            // Задача #30
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    Console.WriteLine("Привет!");
+            //}
+
+            // Задача #31
+            //Console.Write("Введите число: ");
+            //int n = int.Parse(Console.ReadLine());
+            //for (int x = 1; x <= n; x++)
+            //{
+            //    Console.WriteLine("Информатика");
+            //}
+
+            // Задача #32
+            //for (int x = 1; x <= 5; x++)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            // Задача #33
+            //Console.Write("Введите символ: ");
+            //string sym = Console.ReadLine();
+            //for (int x = 0; x <= 18; x++)
+            //{
+            //    Console.Write(sym);
+            //}
+
+            // Задача #34
+            //for (int x = 10; x > 0; x--)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            // Задача - 35
+            //Console.Write("Введите символ: ");
+            //string sym = Console.ReadLine();
+            //for (int i = 0; i < 18; i++)
+            //{
+            //    Console.Write(sym);
+            //}
+            //Console.WriteLine();
+
+            // Задача - 36
+            //for (int x = 1; x <= 20; x += 2)
+            //{
+            //    int y = 5 * x - 2;
+            //    Console.WriteLine($"x = {x}, y = {y}");
+            //}
+
+
+            // Задача - 37
+            //for (int x = 1; x <= 30; x += 3)
+            //{
+            //    int y = 4 * x - 5;
+            //    Console.WriteLine($"x = {x}, y = {y}");
+            //}
+
+
+            // Задача - 38
+            //for (int x = -5; x <= 5; x += 2)
+            //{
+            //    int y = 5 * x * x - 2 * x + 1;
+            //    Console.WriteLine($"x = {x}, y = {y}");
+            //}
+
+
+            // Задача - 39
+            //for (int x = -9; x <= 9; x += 3)
+            //{
+            //    int y = 4 * x * x + 5 * x - 10;
+            //    Console.WriteLine($"x = {x}, y = {y}");
+            //}
+
+
+            // Задача - 40
+            //for (int i = 1; i <= 11; i+=2)
+            //{
+            //    if (i != 3)
+            //    {
+            //        Console.WriteLine(i\t);
+            //    }
+            //}
+
+
+            // Задача - 41
+            //for (int i = 4; i <= 20; i += 4)
+            //{
+            //    Console.Write(i\t);
+            //}
+            //Console.WriteLine();
+
+
+            // Задача - 42
+            //for (int i = 1; i <= 11; i+=2)
+            //{
+            //    if (i != 3)
+            //    {
+            //        Console.WriteLine(i\t);
+            //    }
+            //}
+
+
+            // Задача #43
+            //for (int x = 10; x <= 100; x++)
+            //{
+            //    if (x % 10 == 0)
+            //    {
+            //        Console.WriteLine(x);
+            //    }
+            //}
+
+            // Задача #44
+            //for (int x = 100; x <= 200; x++)
+            //{
+            //    if (x % 3 == 0)
+            //    {
+            //        Console.WriteLine(x);
+            //    }
+            //}
+
+            // Задача #45 
+            //Console.WriteLine("1 товар - 20,4 рублей");
+            //for (double x = 2; x <= 20; x++)
+            //{
+            //    Console.WriteLine($"{x} товаров - {x * 20.4:F2} рублей");
+            //}
+
+            // Задача #46 
+            //for (int x = 0; x <= 10; x++)
+            //{
+            //    Console.Write("20 ");
+            //}
+
+            // Задача 47
+            //Console.Write("Введите курс доллара: ");
+            //double x = double.Parse(Console.ReadLine());
+            //for (int i = 1; i <= 20; i++)
+            //{
+            //    Console.WriteLine($"{i} доллар(ов) = {i * x} рублей");
+            //}
+
+            // Задача 48
+            //int sum = 0;
+            //for (int x = 0; x <= 1000; x++)
+            //{
+            //    if(x % 5 == 0)
+            //    {
+            //        sum = sum + x;
+            //    }
+            //}
+            //Console.WriteLine($"Сумма всех чисел: {sum}");
+
+            // Задача 49
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.Write("10\t");
+            //}
+
+            // Задача 50
+            //for (int x = 10; x<=100; x++)
+            //{
+            //    if(x % 11 == 0)
+            //    {
+            //        Console.WriteLine(x);
+            //    }
+            //}
+            Console.Read();
+        }
+    }
+}
